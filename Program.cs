f80e1ef2958a2dbcb1c6ebcad0bd38cd2d9f2162
LwandisoSihluku12 <lwandisodube9@gmail.com>
2026-04-14 10:14:14 +0200

    Program.cs file to put everything together

--- Program.cs
+++ Program.cs
@@ -1,1 +1,43 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace CybersecurityChatbot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Chatbot chatbot = new Chatbot();
+
+            // 1. Display ASCII Art
+            ConsoleUI.DisplayAsciiArt();
+            Console.WriteLine("\n" + new string('=', 50) + "\n");
+
+            // 2. Greet User and Get Name
+            chatbot.GreetUser();
+            Console.WriteLine("\n" + new string('=', 50) + "\n");
+
+            // 3. Main Chat Loop
+            string userInput;
+            do
+            {
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.Write("You: ");
+                userInput = Console.ReadLine();
+                Console.ResetColor();
+
+                if (!string.IsNullOrWhiteSpace(userInput) && userInput.ToLower() == "exit")
+                {
+                    Console.ForegroundColor = ConsoleColor.Cyan;
+                    ConsoleUI.TypeMessage("Goodbye! Stay safe online!");
+                    Console.ResetColor();
+                    break;
+                }
+
+                chatbot.ProcessUserInput(userInput);
+                Console.WriteLine("\n" + new string('=', 50) + "\n");
+
+            } while (true);
+        }
+    }
+}
+
