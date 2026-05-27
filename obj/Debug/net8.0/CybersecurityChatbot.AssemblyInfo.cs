a7b0b58a321b800d4b816dea77235bd40e9087c9
LwandisoSihluku12 <lwandisodube9@gmail.com>
2026-05-27 09:42:34 +0200

    Run

--- obj/Debug/net8.0/CybersecurityChatbot.AssemblyInfo.cs
+++ obj/Debug/net8.0/CybersecurityChatbot.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CybersecurityChatbot")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+319682fe5526bdf5578239288f8f5e68acd0590b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a0ec7ab2d554ec600f4f9b954ff51cd7ebaedea1")]
 [assembly: System.Reflection.AssemblyProductAttribute("CybersecurityChatbot")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CybersecurityChatbot")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
